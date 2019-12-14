1b4592c86fbe4923ee3c79178b0ccbf56442f514
vicoyes <thevikoyes@gmail.com>
2019-12-13 19:26:36 -0500

    agregando botton menu

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'modelHair.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -75,25 +76,6 @@ class Home extends StatelessWidget {
   }
 }
 
-class Choice {
-  const Choice({this.title, this.id, this.lista});
-
-  final String title;
-  final int id;
-  final  List lista;
-  
-}
-
-const List<Choice> choices = const <Choice>[
-  const Choice(title: 'Popular', id: 45, lista: [
-    'https://www.tekcrispy.com/wp-content/uploads/2017/12/bancos-imagenes-gratis-640x422.jpg',
-    'https://images.unsplash.com/photo-1575859694244-0b337bf58e0b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80'
-    
-  ]),
-  const Choice(title: 'Cabello Corto',  id: 60, lista: ['https://images.unsplash.com/photo-1575859694244-0b337bf58e0b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80']),
-  const Choice(title: 'Cabello Medio', id: 25, lista: ['https://www.tekcrispy.com/wp-content/uploads/2017/12/bancos-imagenes-gratis-640x422.jpg']),
-  const Choice(title: 'Cabello Largo', id: 75, lista: ['https://www.tekcrispy.com/wp-content/uploads/2017/12/bancos-imagenes-gratis-640x422.jpg']),
-];
 
 class ChoiceCard extends StatelessWidget {
   const ChoiceCard({Key key, this.choice}) : super(key: key);
